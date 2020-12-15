48e02a6b0bf82cde1a6556c41ee6aa79b2fbf71e
Jorge <jorgeluisrojasb@gmail.com>
2020-12-14 22:11:20 -0400

    trying to resolve things

--- src/requests/inventory.js
+++ src/requests/inventory.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const BASE_URL = "/api/inventory";
 
 export default {
@@ -37,43 +36,3 @@ export default {
     }
 }
 
-=======
-const BASE_URL = "/api/inventory";
-
-export default {
-    fetchAll: async function () {
-        let inventory = await fetch(BASE_URL, {
-            headers: {
-                "Authorization": "Bearer " + localStorage.getItem("jwt")
-            }
-        })
-
-        let response = await inventory.json();
-
-        return response;
-    },
-    fetchByProductId: async function (productId) {
-        let inventory = await fetch(BASE_URL + `?id=${productId}`, {
-            headers: {
-                "Authorization": "Bearer " + localStorage.getItem("jwt")
-            }
-        })
-
-        let response = await inventory.json();
-
-        return response;
-    },
-    fetchByProductName: async function (name) {
-        let inventory = await fetch(BASE_URL + `?name=${name}`, {
-            headers: {
-                "Authorization": "Bearer " + localStorage.getItem("jwt")
-            }
-        })
-
-        let response = await inventory.json();
-
-        return response;
-    }
-}
-
->>>>>>> 03db5aa56eb18f1d2585aadc5f784647e920b053
